141a1c17c8c19080b124b8012593c9cea7813533
Harsh10092000 <143779029+Harsh10092000@users.noreply.github.com>
2025-03-15 15:26:24 +0530

    commit

--- app/layout.js
+++ app/layout.js
@@ -20,9 +20,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Head>
       <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&amp;family=Nunito:wght@300;400;500;600;700&amp;display=swap" rel="stylesheet" />
-      <link rel="stylesheet" href="/assets/css/plugins/swiper-bundle.min.css" />
+      {/* <link rel="stylesheet" href="/assets/css/plugins/swiper-bundle.min.css" />
         <link rel="stylesheet" href="/assets/css/plugins/glightbox.min.css" />
-        <link rel="stylesheet" href="/assets/css/plugins/aos.css" />
+        <link rel="stylesheet" href="/assets/css/plugins/aos.css" /> */}
       </Head>
       <body>
         <AddBootstrap />
