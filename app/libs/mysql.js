32457990c2d25123a227ca512a0f22694a2f2c50
Harsh10092000 <143779029+Harsh10092000@users.noreply.github.com>
2025-07-19 15:16:17 +0530

    Update mysql.js

--- app/libs/mysql.js
+++ app/libs/mysql.js
@@ -1,12 +1,23 @@
+// import mysql from 'mysql2/promise'
+
+// const pool = mysql.createPool({
+//   host: "194.59.164.60",
+//   user: "u414768521_landmark_pro",
+//   password: "Gw[3H&SEBfT",
+//   database: "u414768521_landmark_pro",
+//   waitForConnections: true,
+// })
+
+// export default pool
+
 import mysql from 'mysql2/promise'
 
 const pool = mysql.createPool({
-  host: "194.59.164.60",
-  user: "u414768521_landmark_pro",
-  password: "Gw[3H&SEBfT",
-  database: "u414768521_landmark_pro",
+  host: "193.203.166.208",
+  user: "u706648698_landmark_plots",
+  password: "6k@Z/*6VN+a9",
+  database: "u706648698_landmark_plots",
   waitForConnections: true,
 })
 
 export default pool
-
